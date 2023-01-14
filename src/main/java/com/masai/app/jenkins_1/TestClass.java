b99d45d049ae91ca51e1da320e31f57bb7677bb5
Kanishka Shrivastava <kanishkamichael9@gmail.com>
2023-01-14 10:12:48 +0530

    Jenkins_Q1

---
+++ src/main/java/com/masai/app/jenkins_1/TestClass.java
@@ -0,0 +1,11 @@
+package com.masai.app.jenkins_1;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello India");
+
+	}
+
+}
---
+++ src/test/java/com/masai/app/jenkins_1/JenkinsQ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.app.jenkins_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsQ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
